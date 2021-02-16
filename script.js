301483fcef968104bb63af660d50880329499ea0
sahildua899 <sahildua899@gmail.com>
2021-02-16 22:30:51 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('Testing');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to Select media stream, pass the video element, then play
+
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        // Catch Error Here
+        console.log('Whoops Error Here:', error)
+    }
+}
+
+button.addEventListener('click', async () => {
+    // Disable Button
+    button.disabled = true;
+    // Start Picture in Picture
+    await videoElement.requestPictureInPicture();
+    // Reset Button
+    button.disabled = false;
+});
+
+// On Load
+selectMediaStream();
